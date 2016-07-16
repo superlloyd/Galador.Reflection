46b94dde289a379848f5f347e7df97670d697e58
superlloyd <ld@galador.net>
2016-07-16 16:37:01 +1000

    moved some code back in the shared project

--- Galador.Reflection.Shared/Logging/TraceKeys.cs
+++ Galador.Reflection.Shared/Logging/TraceKeys.cs
@@ -6,10 +6,38 @@ using System.Text;
 using System.Collections.Concurrent;
 using System.Collections;
 
+#if __NET__
+
+using System.Configuration;
+
+#endif
+
 namespace Galador.Reflection.Logging
 {
-    public static partial class TraceKeys
+    public static class TraceKeys
     {
+#if __NET__
+
+        static TraceKeys()
+        {
+            var prefix = "TraceKeys.";
+            var allkeys = ConfigurationManager.AppSettings.AllKeys.Where(x => x.StartsWith(prefix));
+            foreach (var key in allkeys)
+            {
+                var name = key.Substring(prefix.Length);
+                var svalue = ConfigurationManager.AppSettings[key];
+                var value = string.Compare(svalue, "true", true) == 0
+                    || string.Compare(svalue, "on", true) == 0
+                    || string.Compare(svalue, "enable", true) == 0
+                    || string.Compare(svalue, "enabled", true) == 0
+                    || string.Compare(svalue, "1", true) == 0
+                    ;
+                TraceKeys.Traces[name].IsEnabled = value;
+            }
+        }
+
+#endif
+
         public class TracesProperty : IEnumerable<TraceKey>
         {
             ConcurrentDictionary<string, TraceKey> traces = new ConcurrentDictionary<string, TraceKey>();
